97a979d7a0807cd3b2889e3e8d6c02356f2b5265
Julian <julianbattaglino@gmail.com>
2019-01-06 17:55:05 -0300

    Fix Spinner Items Actions

--- app/src/main/java/com/example/julian/syloperligas/Launcher.java
+++ app/src/main/java/com/example/julian/syloperligas/Launcher.java
@@ -1,6 +1,5 @@
 package com.example.julian.syloperligas;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,11 +10,11 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.Toast;
-
+import android.widget.TextView;
 
 public class Launcher extends AppCompatActivity {
 
+    boolean isLigaSelected = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,57 +22,47 @@ public class Launcher extends AppCompatActivity {
         setContentView(R.layout.launcher);
 
 
-        Spinner spinner;
-        spinner = findViewById(R.id.spinner);
+        TextView poweredby = findViewById(R.id.powered);
+        TextView dharma = findViewById(R.id.dharma);
+
+
         ImageView syloperlogo = findViewById(R.id.syloperlogo);
         CardView cardviewspinner = findViewById(R.id.cardviewspinner);
-        CardView cardviewseleccionar = findViewById(R.id.seleccionar);
+        Spinner spinner = findViewById(R.id.spinner);
+
+        AnimationUtils.enterTop(syloperlogo, 1000);
+        AnimationUtils.landMe(syloperlogo, 2000);
+
+        AnimationUtils.enterLeft(cardviewspinner, 1500);
+
+        AnimationUtils.enterLeft(poweredby, 2200);
+        AnimationUtils.enterLeft(dharma, 2000);
+        AnimationUtils.rotateX(dharma, 2200);
 
 
-        AnimationUtils.enterTop(syloperlogo, 900);
-        AnimationUtils.landMe(syloperlogo, 1500);
-        AnimationUtils.landMeX(syloperlogo, 2000);
-        AnimationUtils.landMeY(syloperlogo, 2500);
-
-
-
-
-        AnimationUtils.enterRight(cardviewseleccionar, 1000);
-
-        AnimationUtils.enterLeft(cardviewspinner, 1100);
-
-
-
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(Launcher.this, R.layout.support_simple_spinner_dropdown_item,
-                getResources().getStringArray(R.array.spinner_ligas));
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-
+        spinner.setAdapter(new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.spinner_divisiones)));
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
-                Toast.makeText(adapterView.getContext(),
-                        (String) adapterView.getItemAtPosition(pos), Toast.LENGTH_SHORT).show();
+                if (isLigaSelected) {
+                    openMainActivity(pos);
+                }
+                isLigaSelected = true;
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-            }
-
-
-        });
-
-        CardView Seleccionar = findViewById(R.id.seleccionar);
-        Seleccionar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Launcher.this, MainActivity.class);
-                startActivity(i);
+                //
             }
         });
+    }
+
+    private void openMainActivity(int pos) {
+        //Toast.makeText(adapterView.getContext(), (String) adapterView.getItemAtPosition(pos), Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(Launcher.this, MainActivity.class);
+        i.putExtra("division", pos);
+        startActivity(i);
     }
 }
 
